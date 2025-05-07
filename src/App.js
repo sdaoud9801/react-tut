626ed57834c555952b5e6ad6ef39aced0ab6e3ed
sdaoud980 <sdaoud980@gmail.com>
2025-05-07 21:47:50 +0300

    what

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
+  function handleNameChanges(){
+    let names = ["Salah","Bob","Karina"];
+    let name = names[Math.floor(Math.random() * 3)];
+    return name;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
